fa8ebbf760528a9a35491ac41a45ff8708cb805f
hemant052004 <kashyaphemant2004@gmail.com>
2024-09-14 17:58:24 +0530

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../atoms/Navbar';
+
+const features = [
+  { title: 'Growing Revenue', description: 'Start with signing up.' },
+  { title: 'Best in Class Content', description: 'High-quality lessons.' },
+  { title: 'Competitive Advantage', description: 'Gain an edge.' },
+  { title: 'Growth Potential', description: 'Unlock new opportunities.' },
+];
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/atoms/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-24 border-b-2 border-black bg-white pl-14 pr-14 pt-4 flex'>
+      
+      <div className='w-[40%] h-full'>
+
+        <h1 className='text-[40px] font-[700]'>Learvibe</h1>
+      </div>
+      <div className='w-[60%] h-full border border-black'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
